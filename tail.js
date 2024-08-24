cb93d5ef41e8b8c6160a17b828def512868d85e3
ExperCodeVaibhav <vaibhavsri2004@gmail.com>
2024-08-24 11:47:34 +0530

    Initial commit of portfolio project

---
+++ tail.js
@@ -0,0 +1,12 @@
+gsap.from('#box', {
+    x: 500,
+    duration:5,
+    delay:2,
+    y:500,
+    backgroundColor:'blue',
+    width:100,
+    rotate:360,
+    repeat:1,
+    yoyo:true,
+    stagger:4
+});
